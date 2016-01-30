686a21ac4297159e352af37b01075e48eca587e2
YMHuang <ym.huang0808@gmail.com>
2016-01-30 20:58:11 +0800

    Correct .gitignore

---
+++ src/generators/app/index.js
@@ -0,0 +1,25 @@
+import { Base } from 'yeoman-generator'
+
+export default class MyGenerator extends Base {
+  get prompting() {
+    return {
+
+      appName() {
+        let done = this.async()
+        let prompt = [
+          {
+            type: 'input',
+            name: 'appName',
+            message: 'Enter a name for your app:',
+          },
+        ]
+
+        this.prompt(prompt, ( { appName} ) => {
+          this.options.appName = appName
+          done()
+        })
+      }
+      
+    }
+  }
+}
