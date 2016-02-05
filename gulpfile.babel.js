78673e4ed957486166959c29312cc36b6a00a04e
YMHuang <ym.huang0808@gmail.com>
2016-02-05 11:41:14 +0800

    generators gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,70 @@
+'use strict'
+
+import path from 'path'
+import gulp from 'gulp'
+import gulpLoadPlugins from 'gulp-load-plugins'
+import del from 'del'
+
+const $ = gulpLoadPlugins({
+  rename: {
+    'gulp-exclude-gitignore': 'excludeGitignore',
+    'gulp-jsx-coverage': 'jsxCoverage'
+  }
+})
+
+// Lint JavaScript
+gulp.task('lint', () =>
+  gulp.src('./src/**/*.js')
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failOnError())
+)
+
+// Coverage
+gulp.task('coverage', () => {
+  let options = {
+      istanbul: {
+        preserveComments: true,
+        coverageVariable: '__MY_TEST_COVERAGE__',
+        exclude: /node_modules/
+      },
+      transpile: {
+        babel: {
+          include: /\.js?$/,
+          exclude: /node_modules/,
+          omitExt: false
+        }
+      },
+      coverage: {
+        reporters: ['text-summary', 'json', 'lcov'],
+        directory: 'coverage'
+      }
+  }
+
+  $.jsxCoverage.initModuleLoaderHack(options)
+
+  return gulp.src('./src/**/*.js')
+    .pipe($.excludeGitignore())
+    .pipe($.jasmine())
+    .on('end', $.jsxCoverage.collectIstanbulCoverage(options))
+  }
+)
+
+// Babel transformation
+gulp.task('scripts', () =>
+  gulp.src('./src/**/*.js')
+    .pipe($.changed('./generators'))
+    .pipe($.excludeGitignore())
+    .pipe($.babel())
+    .pipe(gulp.dest('.'))
+)
+
+
+// Clean scripts
+gulp.task('clean:scripts', () =>
+  del('./generators').then(paths =>
+    $.util.log('Clean ./generators directory')
+  )
+)
+
+gulp.task('build', ['clean:scripts', 'lint', 'scripts'])
